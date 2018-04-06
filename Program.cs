442c375dacadb21ce2776003e92ed1641e19cc25
alan-pecero <36742269+alan-pecero@users.noreply.github.com>
2018-04-06 10:23:35 -0500

    Upload Changes
    Program, libExcel_epp and Support, "Run" Column functionality is ready.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using PruebaExcel_EPplus;
+using OfficeOpenXml;
+
+
+namespace SeleniumExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driverFF = new FirefoxDriver(@"C:\geckodriver-v0.19.1-win64");
+            libExcel_epp objeto_Excel = new libExcel_epp();
+            Support objeto_Support = new Support("WorkbookSelenium", "Sheet1", driverFF, objeto_Excel);
+            objeto_Support.SearchGoogle();
+        }
+    }
+}
